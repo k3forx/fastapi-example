941e8a277dff99dd255f675f6ce5b3219df827b5
k3forx <kanata.miyahana@bebit.com>
2021-04-12 22:50:59 +0900

    update

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ After containers successfully run, you can check an endpoint with `curl` command
 > curl localhost:8002/ping
 {"ping":"pong!"}%
 
+> curl localhost:8002/notes/1/
+{"id":1,"title":"Beyond the legacy code","description":"Awesome book!"}%
+
+> curl localhost:8002/notes/2/
+{"detail":"Note not found"}%
+
 ```
 ## Unit test
 
--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.db import mysql
 
 router = APIRouter()
@@ -7,5 +7,10 @@ router = APIRouter()
 @router.get("/{note_id}/")
 def get_note_by_id(note_id: int):
     query = f"SELECT * FROM notes WHERE id = {note_id};"
-    result = mysql.execute_fetch_query(query)
-    return result
+    try:
+        result = mysql.execute_fetch_query(query)[0]
+        response = {"id": result[0],
+                    "title": result[1], "description": result[2]}
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Note not found")

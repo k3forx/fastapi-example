a3d7d16341407bed524e02cd805b2918892e17d0
k3forx <kanata.miyahana@bebit.com>
2021-04-17 22:27:46 +0900

    fix error

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,11 @@
-from starlette_prometheus import PrometheusMiddleware, metrics
+from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from app.api import notes, ping
 from fastapi import FastAPI
 
 app = FastAPI()
 app.add_middleware(PrometheusMiddleware)
-app.add_route("/metrics", metrics)
+app.add_route("/metrics", handle_metrics)
 
 app.include_router(ping.router)
 app.include_router(notes.router, prefix="/notes", tags=["notes"])

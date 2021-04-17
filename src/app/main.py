4f6f65ed1ee40ce5ea7be0cb6fb0b3ddfc4c9acc
Kanata Miyahana <afiri.bupa@gmail.com>
2021-04-17 12:34:06 +0900

    Expose metrics (#22)

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,11 @@
+from starlette_prometheus import PrometheusMiddleware, metrics
+
+from app.api import notes, ping
 from fastapi import FastAPI
-from app.mysql_client import mysql
-from app.api import ping, notes
 
 app = FastAPI()
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
 
 app.include_router(ping.router)
 app.include_router(notes.router, prefix="/notes", tags=["notes"])

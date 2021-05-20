87305f9fae97f52ecc778543493ce855e0dac91f
Kanata Miyahana <afiri.bupa@gmail.com>
2021-05-21 00:11:13 +0900

    Add an endpoint for health check (#98)

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,6 @@
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 
-from app.api import notes, ping
+from app.api import healthcheck, notes, ping
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +11,4 @@ app.add_route("/metrics", handle_metrics)
 
 app.include_router(ping.router)
 app.include_router(notes.router, prefix="/notes", tags=["notes"])
+app.include_router(healthcheck.router)

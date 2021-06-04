5f6b5a6362bb806b208823605a240a2008bff319
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-04 23:48:07 +0900

    Remove 'ping' endpoint

--- app/api/ping.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse
-
-router = APIRouter()
-
-
-@router.get("/ping")
-def pong():
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"ping": "pong!"})
